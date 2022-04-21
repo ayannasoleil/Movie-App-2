450bc5cb7555743c20676d1af30c3efaf6146a95
Ayanna Thomas <ayannathomas@ayannas-mbp.myfiosgateway.com>
2022-04-20 23:24:31 -0400

    updated changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export const MovieCard = ({ movie }) => {
-  return (
+  
+  const removeMovieFromFavorites = () => {
+    
+  }
+    return (
     <div className="movie-card">
       <div className="overlay"></div>
+      <div className="inner-card-controls">
+        <button className="ctrl-btn" onClick={removeMovieFromFavorites}>
+          <i className='fa-fw fa fa-times'></i>
+        </button>
 
+      </div>
       <img src={`https://image.tmdb.org/t/p/w500/${movie.card}`} alt={`${movie.title} Poster`} />
     </div>
   )
-}
+};
